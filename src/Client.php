d9f4c613d950cc784ce536372fc9b711bd820d67
Carl Alexander <carlalexander@gmail.com>
2024-02-08 14:07:46 -0700

    chore: bump version

--- src/Client.php
+++ src/Client.php
@@ -925,7 +925,7 @@ final class Client implements ClientInterface
         $headers = [
             'Accept' => 'application/json',
             'Content-Type' => 'application/json',
-            'User-Agent' => 'ymir-sdk-php/1.1.0',
+            'User-Agent' => 'ymir-sdk-php/1.2.0',
         ];
         $method = strtolower($method);
 
