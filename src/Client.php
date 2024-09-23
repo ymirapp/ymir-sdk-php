c224b311480eb2d4f709c3aae5f5565bfa1497c0
Carl Alexander <carlalexander@gmail.com>
2024-09-23 12:32:34 -0400

    ci: ran `php-cs-fixer fix`

--- src/Client.php
+++ src/Client.php
@@ -644,10 +644,10 @@ final class Client implements ClientInterface
     public function getEnvironmentLogs(int $projectId, string $environment, string $function, int $since, ?string $order = null): Collection
     {
         return $this->request('get', "/projects/{$projectId}/environments/{$environment}/logs?".http_build_query([
-                'function' => $function,
-                'since' => $since,
-                'order' => $order,
-            ]));
+            'function' => $function,
+            'since' => $since,
+            'order' => $order,
+        ]));
     }
 
     /**
